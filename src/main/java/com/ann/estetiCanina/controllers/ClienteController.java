1ce36c4b8cf87397890079f783b6d4cf82f40804
Paola VC <147096999+PaolaVC08@users.noreply.github.com>
2025-06-07 18:23:17 -0600

    Update ClienteController.java

--- src/main/java/com/ann/estetiCanina/controllers/ClienteController.java
+++ src/main/java/com/ann/estetiCanina/controllers/ClienteController.java
@@ -7,6 +7,7 @@ import com.ann.estetiCanina.service.interfaces.IClienteService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import java.util.List;
 @RestController
 @RequestMapping("/api/clientes")
 @CrossOrigin(origins = "*")
