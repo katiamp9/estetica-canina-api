06537d242c711570559bd299a4fd33d65a46c247
Paola VC <147096999+PaolaVC08@users.noreply.github.com>
2025-06-07 18:21:20 -0600

    Update IClienteService.java

--- src/main/java/com/ann/estetiCanina/service/interfaces/IClienteService.java
+++ src/main/java/com/ann/estetiCanina/service/interfaces/IClienteService.java
@@ -2,7 +2,7 @@ package com.ann.estetiCanina.service.interfaces;
 
 import com.ann.estetiCanina.dtos.response.ClienteResponse;
 import com.ann.estetiCanina.dtos.request.ClienteRequest;
-
+import java.util.List;
 
 public interface IClienteService {
     ClienteResponse registrarCliente(ClienteRequest request);
